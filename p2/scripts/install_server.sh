8f8405abafb68c4581a86ade8aa5ee0bca7da3ff
Gap <132090220+Gpatrix@users.noreply.github.com>
2025-10-06 13:24:37 +0200

    [WIP]: ingress

--- p2/scripts/install_server.sh
+++ p2/scripts/install_server.sh
@@ -1,11 +1,10 @@
-PATH="/home/vagrant"
+VAGRANT_PATH="/home/vagrant";
 
-mv ${PATH}/confs/config.yaml /etc/rancher/k3s/config.yaml
+cp ${VAGRANT_PATH}/confs/config.yaml /etc/rancher/k3s/config.yaml
 
 echo "Installing k3s in server mode..."
 curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE=644 K3S_TOKEN=mynodetoken sh -
 
 apt install kubectl
 
-kubectl apply -f ${PATH}/confs/apps.yaml
-kubectl apply -f ${PATH}/confs/ingress.yaml
+kubectl apply -f ${VAGRANT_PATH}/confs/apps.yaml
