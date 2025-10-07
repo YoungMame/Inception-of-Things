646f2a8b9ac9c78afd000f7a04fb49d16f529f2f
mduvey <mduvey@IoT2.myguest.virtualbox.org>
2025-10-07 16:03:35 +0200

    add:(wip:(BUG argocd panel forward))

--- p3/scripts/create-cluster.sh
+++ p3/scripts/create-cluster.sh
@@ -5,4 +5,12 @@ else
     kubectl apply -f ./dev/namespace.yaml
     kubectl apply -f ./argocd/namespace.yaml
     kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml > /dev/null
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout tls.key -out tls.crt -subj "/CN=argocd.example.com"
+
+    kubectl create secret tls argocd.example.com \
+    --cert=tls.crt --key=tls.key -n argocd
+    kubectl apply -f ./argocd/ingress.yaml
+
+    kubectl config set-context --current --namespace=argocd
 fi
