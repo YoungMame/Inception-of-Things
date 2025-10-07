b276f6bfedd9351c282ffb03523facc09eb01b0a
mduvey <mduvey@IoT2.myguest.virtualbox.org>
2025-10-07 12:22:32 +0200

    add:(launch k3d script)

--- p3/scripts/create-cluster.sh
+++ p3/scripts/create-cluster.sh
@@ -1,2 +1,8 @@
-k3d cluster create --config p3/cluster.yaml
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+if [ "$(k3d cluster list | grep -c mycluster)" -gt 0 ]; then
+    echo "Cluster mycluster already exists."
+else
+    k3d cluster create --config ./dev/cluster.yaml
+    kubectl apply -f ./dev/namespace.yaml
+    kubectl apply -f ./argocd/namespace.yaml
+    kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml > /dev/null
+fi
