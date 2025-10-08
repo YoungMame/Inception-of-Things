9bf80d922ea3dc8ac2b64a1daf78fd01b256174c
Gap <132090220+Gpatrix@users.noreply.github.com>
2025-10-08 12:12:58 +0200

    [ADD]: will42 repo lanch

--- p3/scripts/init.sh
+++ p3/scripts/init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo k3d cluster delete p3-cluster
 sudo k3d cluster create p3-cluster
 
 sudo kubectl create namespace argocd
@@ -8,12 +9,18 @@ sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-
 sudo kubectl config set-context --current --namespace=argocd
 sudo argocd login --insecure --core
 
+# TODO wait for status=ready
 sudo kubectl port-forward svc/argocd-server -n argocd 8080:443 &
 
 # default admin password
 echo admin pasword: $(sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d)
 
 
-kubectl config set-context --current --namespace=argocd
-argocd app create guestbook --repo https://github.com/argoproj/argocd-example-apps.git --path guestbook --dest-server https://kubernetes.default.svc --dest-namespace default
-argocd app sync guestbook
+sudo kubectl config set-context --current --namespace=argocd
+
+sudo argocd app create will42 --insecure --repo https://github.com/Gpatrix/42.Inception-of-Things.manifests.git --path main --dest-server https://kubernetes.default.svc --dest-namespace default
+sudo argocd app sync will42
+
+# TODO better way ?
+sudo kubectl port-forward svc/will42 -n default 8888:8888
+# curl localhost:8888
