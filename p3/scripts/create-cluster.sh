adf99a43e9f96ef0d0028d7a96239fb4ccebd7d3
mduvey <mduvey@IoT2.myguest.virtualbox.org>
2025-10-08 08:18:52 +0200

    WIP:argocd ingress

--- p3/scripts/create-cluster.sh
+++ p3/scripts/create-cluster.sh
@@ -5,12 +5,13 @@ else
     kubectl apply -f ./dev/namespace.yaml
     kubectl apply -f ./argocd/namespace.yaml
     kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml > /dev/null
+
+    # TODO wait for argocd pod to be ready
+
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
     -keyout tls.key -out tls.crt -subj "/CN=argocd.example.com"
-
     kubectl create secret tls argocd.example.com \
     --cert=tls.crt --key=tls.key -n argocd
+    rm tls.crt tls.key
     kubectl apply -f ./argocd/ingress.yaml
-
-    kubectl config set-context --current --namespace=argocd
 fi
