179ee449b94084cedd65c1c40397b7912a6a10dd
mduvey <mduvey@IoT2.myguest.virtualbox.org>
2025-10-08 14:24:13 +0200

    add:()namespace dev and app sync policy

--- p3/scripts/init.sh
+++ p3/scripts/init.sh
@@ -4,23 +4,30 @@ sudo k3d cluster delete p3-cluster
 sudo k3d cluster create p3-cluster
 
 sudo kubectl create namespace argocd
+sudo kubectl create namespace dev
 sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+# Wait for apply to finish, wait for pods to be ready
+# sudo kubectl wait --for=condition=Ready pod -l app.kubernetes.io/name=argocd-server -n argocd --timeout=300s
+sleep 20
 
 sudo kubectl config set-context --current --namespace=argocd
 sudo argocd login --insecure --core
 
-# TODO wait for status=ready
 sudo kubectl port-forward svc/argocd-server -n argocd 8080:443 &
+sleep 10
 
 # default admin password
-echo admin pasword: $(sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d)
+echo admin password: $(sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d)
 
 
 sudo kubectl config set-context --current --namespace=argocd
 
-sudo argocd app create will42 --insecure --repo https://github.com/Gpatrix/42.Inception-of-Things.manifests.git --path main --dest-server https://kubernetes.default.svc --dest-namespace default
+sudo argocd app create will42 --insecure --repo https://github.com/Gpatrix/42.Inception-of-Things.manifests.git --path main --dest-server https://kubernetes.default.svc --dest-namespace dev
 sudo argocd app sync will42
+argocd app set will42 --sync-policy automated
+argocd app set will42 --auto-prune
 
 # TODO better way ?
-sudo kubectl port-forward svc/will42 -n default 8888:8888
+sudo kubectl port-forward svc/will42 -n dev 8888:8888
 # curl localhost:8888
